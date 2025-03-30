d592d31ed9a35a992672e6cc6914deff31198d2d
Justin Bowen <JusBowen@gmail.com>
2025-03-30 11:41:47 -0700

    Adding proper dummy rails test app

---
+++ lib/tasks/activeagent_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :activeagent do
+#   # Task goes here
+# end
---
+++ test/activeagent_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ActiveagentTest < ActiveSupport::TestCase
+  test "it has a version number" do
+    assert Activeagent::VERSION
+  end
+end
