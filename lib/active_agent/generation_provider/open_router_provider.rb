b61dfdcd76324701bcc95ad9dc2348246bcff0e7
Krzysztof Hasiński <krzysztof.hasinski@gmail.com>
2025-04-18 08:00:48 +0900

    Add OpenRouter support

---
+++ lib/active_agent/generation_provider/open_router_provider.rb
@@ -0,0 +1,17 @@
+# lib/active_agent/generation_provider/openrouter_provider.rb
+
+require "openai"
+require_relative "open_ai_provider"
+
+module ActiveAgent
+  module GenerationProvider
+    class OpenRouterProvider < OpenAIProvider
+      def initialize(config)
+        @config = config
+        @api_key = config["api_key"]
+        @model_name = config["model"]
+        @client = OpenAI::Client.new(uri_base: "https://openrouter.ai/api/v1", access_token: @api_key, log_errors: true)
+      end
+    end
+  end
+end
